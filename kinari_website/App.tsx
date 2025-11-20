51febd3cd67a2f8c39ec5e1e7f1e83a131d12379
ebenarous <eliorb@hotmail.fr>
2025-11-20 04:57:48 +0100

    new

---
+++ kinari_website/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Hardware from './components/Hardware';
+import Capabilities from './components/Capabilities';
+import Demo from './components/Demo';
+import Deploy from './components/Deploy';
+import Footer from './components/Footer';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-black text-white selection:bg-blue-500/30">
+      <Navbar />
+      <main className="flex flex-col w-full">
+        <Hero />
+        <Hardware />
+        <Capabilities />
+        <Demo />
+        <Deploy />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
